ae858cb655f0a417f0f40d96ce1e9805f16be6ae
Pedro Alves <pedrost00700@gmail.com>
2023-12-18 15:45:35 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { create } from 'domain'
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default EstiloGlobal
