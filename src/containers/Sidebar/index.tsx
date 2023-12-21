8a56f63fb5ff8df1fc28962a2eea6e94db9c4f02
Pedro Alves <pedrost00700@gmail.com>
2023-12-21 16:21:17 -0300

    construindo a estrutura e seção sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/pedrosttt.png" />
+    <Titulo fontSize={20}>Pedro Sturmer</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+      accusamus fugiat ex sed earum est fuga, laudantium provident dolorem
+      tempora dignissimos repudiandae voluptatum natus suscipit, obcaecati unde!
+      Temporibus, earum! Odit.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=pedrosttt&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=pedrosttt&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
