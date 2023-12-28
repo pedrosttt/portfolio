2036d08d6e11f0b1fb620ffcf66ee2a3658a265e
Pedro Alves <pedrost00700@gmail.com>
2023-12-28 15:04:19 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/pedrosttt.png" />
-    <Titulo fontSize={20}>Pedro Sturmer</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Pedro Sturmer</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        pedrosttt
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante EBAC
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
